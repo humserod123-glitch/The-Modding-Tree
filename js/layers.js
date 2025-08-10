c3c822bceb077f87c0dac19fc2fb13080d4dbf7a
GrevlingNinja <humserod123@gmail.com>
2025-08-10 02:29:44 +0200

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,12 +1,12 @@
 addLayer("p", {
-    name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
-    symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
+    name: "Honey Badger", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "HB", // This appears on the layer's node. Default is the id with the first letter capitalized
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#4BDC13",
+    color: "#eef106ff",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Honey Badger Points", // Name of prestige currency
     baseResource: "points", // Name of resource prestige is based on
@@ -24,5 +24,12 @@ addLayer("p", {
     hotkeys: [
         {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
     ],
-    layerShown(){return true}
+    layerShown(){return true},
+        upgrades: {        11: {    title: "Honey",
+    description: "Double your point gain.",
+    cost: new Decimal(1),
+
+        },
+
+    },
 })
